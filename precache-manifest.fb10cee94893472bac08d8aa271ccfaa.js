0e2b7e3bb606955f2ac671bee084e2811ebf52ba
katrinrodkina <offthecliff46@gmail.com>
2019-09-16 17:35:59 -0400

    Updates

---
+++ precache-manifest.fb10cee94893472bac08d8aa271ccfaa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1373f8363554d8c360f1eb5dc6189f2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "25136be9b78948a5eaaf",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "52cff111383e7337b107",
+    "url": "/portfolio/static/css/main.cb67c086.chunk.css"
+  },
+  {
+    "revision": "25136be9b78948a5eaaf",
+    "url": "/portfolio/static/js/2.f7b53dce.chunk.js"
+  },
+  {
+    "revision": "52cff111383e7337b107",
+    "url": "/portfolio/static/js/main.befc9e74.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "f9437c438d1c114a47fbb7ed79887337",
+    "url": "/portfolio/static/media/KatrinaRodkina-Resume.f9437c43.pdf"
+  },
+  {
+    "revision": "ebb86f63bfb77505dc1c10a4cdd9ac89",
+    "url": "/portfolio/static/media/avatar.ebb86f63.png"
+  },
+  {
+    "revision": "8e49f17f6f5a02d213d9501726577cfc",
+    "url": "/portfolio/static/media/foody.8e49f17f.png"
+  },
+  {
+    "revision": "f78cda5a99cc472a7290b0918970bfb4",
+    "url": "/portfolio/static/media/mogsy.f78cda5a.png"
+  },
+  {
+    "revision": "e9499dc31230d4b57c98c1a4d865eef8",
+    "url": "/portfolio/static/media/timey.e9499dc3.png"
+  },
+  {
+    "revision": "a3082cf750e50528c9a38cfb1b3af187",
+    "url": "/portfolio/static/media/trivia.a3082cf7.png"
+  }
+]);
